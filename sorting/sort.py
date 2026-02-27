dca3e9e8ade6bb9436d06769d083a315df3dbd09
QuantumKya <rohan.vats4nier@gmail.com>
2026-02-26 21:26:36 -0600

    buggy :ε

--- sorting/sort.py
+++ sorting/sort.py
@@ -27,6 +27,7 @@ class Sort:
         self.width: int = w
         self.height: int = h
         self.pos: int = 0
+        self.lastpos: int = 0
         
         self.verified: bool = True
         self.done: int = 0
@@ -44,81 +45,103 @@ class Sort:
         
         matrix.npxl.fill((0, 0, 0))
         
-        arrlen: int = len(self.arr)
+        arrlen = len(self.arr)
         for i in range(arrlen):
             color = (255, 255, 255)
             
             if self.done >= 1:
                 if i <= self.pos: color = (0, 255, 0)
-            elif i == self.pos: color = (255, 255, 0)
+                if self.pos <= self.lastpos:
+                    self.pos += 1
+                    self.lastpos = self.pos
             
             val = self.arr[i]
             for y in range(self.height):
-                matrix[arrlen-i-1, y] = color if y <= val else (0, 0, 0)
+                matrix[arrlen-i-1, y] = (color if y <= val else (0, 0, 0))
 
+class StepSort(Sort):
+    def draw(self, matrix):
+        Sort.draw(self, matrix)
+        
+        arrlen: int = len(self.arr)
+        color = (0, 255, 0)
+            
+        val = self.arr[self.pos]
+        for y in range(self.height):
+            matrix[arrlen-self.pos-1, y] = color if y <= val else (0, 0, 0)
+    
+    def step(self):
+        Sort.step(self)
+        
+        if self.pos < len(self.arr) - 1:
+            self.process()
+            self.pos += 1
+        else:
+            self.pos = 0
+            if self.verified:
+                self.done += 1
+
+    def process(self):
+        pass
 
 
 # ================================ SORT MAKING !!! ================================ #
 
-class BubbleSort(Sort):
-    def step(self):
-        Sort.step(self)
+class BubbleSort(StepSort):
+    def process(self):
+        current: int = self.arr[self.pos]
+        test: int = self.arr[self.pos+1]
         
-        if self.pos < len(self.arr) - 1:
-            current: int = self.arr[self.pos]
-            test: int = self.arr[self.pos+1]
-            
-            if test < current:
-                self.arr[self.pos+1] = current
-                self.arr[self.pos] = test
-            
-            self.pos += 1
-        else:
-            self.pos = 0
-            if self.verified:
-                self.done += 1
+        if test < current:
+            self.arr[self.pos+1] = current
+            self.arr[self.pos] = test
 
-class StalinSort(Sort):
-    def step(self):
-        Sort.step(self)
+class QuickSort(Sort):
+    def __init__(self, w, h):
+        Sort.__init__(self, w, h)
         
-        if self.pos < len(self.arr) - 1:
-            current: int = self.arr[self.pos]
-            test: int = self.arr[self.pos+1]
-            
-            if test < current:
-                self.arr.pop(self.pos+1)
-            
-            self.pos += 1
-        else:
-            self.pos = 0
-            if self.verified:
-                self.done += 1
+        self.lo = 0
+        self.hi = len(self.arr) - 1
+    
+    def draw(self, matrix):
+        Sort.draw(self, matrix)
+        
+        
+    
+    def step(self):
+        pass
 
-class IngsocSort(Sort):
-    def step(self):
-        Sort.step(self)
+
+
+# ================================ JOKE SORTS ================================ #
+
+class StalinSort(StepSort):
+    def process(self):
+        current: int = self.arr[self.pos]
+        test: int = self.arr[self.pos+1]
         
-        if self.pos < len(self.arr) - 1:
-            current: int = self.arr[self.pos]
-            test: int = self.arr[self.pos+1]
-            
-            diff: int = test - current
-            if diff < -3:
-                self.arr.pop(self.pos+1)
-            elif diff < 0:
-                self.arr[self.pos+1] += 1
-            
-            self.pos += 1
-        else:
-            self.pos = 0
-            if self.verified:
-                self.done += 1
+        if test < current:
+            self.arr.pop(self.pos+1)
+
+class IngsocSort(StepSort):
+    def process(self):
+        current: int = self.arr[self.pos]
+        test: int = self.arr[self.pos+1]
+        
+        diff: int = test - current
+        if diff < -3:
+            self.arr.pop(self.pos+1)
+        elif diff < 0:
+            self.arr[self.pos+1] += 1
 
 class MiracleSort(Sort):
     def step(self):
         if self.verified:
             self.done += 1
+
+class GaslightSort(Sort):
+    def step(self):
+        self.done = 1
 
 class BogoSort(Sort):
     def step(self):
@@ -129,17 +152,31 @@ class BogoSort(Sort):
         else:
             self.pos = -1
 
-class DarwinSort(Sort):
-    def step(self):
-        Sort.step(self)
-
-class QuickSort(Sort):
+class SortSort(StepSort):
     def __init__(self, w, h):
         Sort.__init__(self, w, h)
         
-        self.lo = 0
-        self.hi = len(self.arr) - 1
+        self.newarr = []
+        self.target = 0
+        self.compensate = 0
     
+    def draw(self, matrix):
+        StepSort.draw(self, matrix)
+    
+    def process(self):
+        if self.pos == 0: self.compensate = 0
+        i: int = self.pos-self.compensate
+        
+        if self.arr[self.pos] == self.target:
+            self.newarr.append(self.arr.pop(i))
+            self.compensate += 1
+            return
+        
+        if self.newarr == sorted(self.newarr):
+            self.arr.append(self.newarr[i])
+            self.compensate += 1
+
+class DarwinSort(Sort):
     def step(self):
         Sort.step(self)
 
