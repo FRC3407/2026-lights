f82835bcd514e701b2f006746fc0dde1ea595842
ahumanhuman <wyatt.schwick@gmail.com>
2026-03-21 16:31:01 -0500

    add a sorting algorithm visualization (it does look/work that well)

--- SortingVisual.py
+++ SortingVisual.py
@@ -1,6 +1,4 @@
 import pixelstrip
-import board
-import time
 import random
 import math
 from colors import *
@@ -10,6 +8,8 @@ class SortingVisualAnimation(pixelstrip.Animation):
 	width: int
 	height: int
 	
+	idx: int = 0
+
 	FPS: int
 	
 	number: list[int]
@@ -25,22 +25,54 @@ class SortingVisualAnimation(pixelstrip.Animation):
 		
 	def init(self):
 		self.numbers = []
-		for i in range(self.height):
-			self.numbers.append(i)
-		
-	def draw(self, strip, _delta_time):
+		self.idx = 0
+		for i in range(self.width):
+			self.numbers.append(math.floor(self.height*(i/self.width))+1)
+		self.shuffle()
+
+	def reset(self, strip):
+		self.timeout = 1 / self.FPS
+		self.init()
 		strip.clear()
+
+	def shuffle(self):
+		for _ in range(50):
+			first_idx = random.randint(1, len(self.numbers)) - 1
+			second_idx = random.randint(1, len(self.numbers)) - 1
+			if first_idx == second_idx:
+				continue
+			self.numbers[first_idx], self.numbers[second_idx] = self.numbers[second_idx], self.numbers[first_idx]
+		print(self.numbers)
 		
-		column_width: int = math.floor(self.width/self.height)
-		for i in range(len(self.numbers)):
-			number: int = self.numbers[i]
-			strip.draw_rect(i*column_width, self.height, column_width, -number, True)
-		strip[column_width,1] = PURPLE
+	def update(self):
+		cur: int = self.numbers[self.idx]
+		next: int = self.numbers[self.idx + 1]
 		
-		strip.show()
-		time.sleep(1 / self.FPS)
-		
-		
-		
-		
-	
+		if cur > next:
+			self.numbers[self.idx + 1] = cur
+			self.numbers[self.idx] = next
+
+		self.idx += 1
+		self.idx %= len(self.numbers) - 1
+  
+		if self.idx == 0 and sorted(self.numbers) == self.numbers:
+			self.reset({})
+
+
+	def draw(self, strip, _delta_time):
+		if self.is_timed_out():
+			self.timeout = 1 / self.FPS
+			
+			self.update()
+
+			# strip.clear()
+			column_width: int = math.floor(self.width/self.height)
+			for i in range(len(self.numbers)):
+				for j in range(8):
+					strip[i,j] = WHITE if j < self.numbers[i] else BLACK
+			for j in range(abs(8 - self.numbers[self.idx])):
+				strip[self.idx, abs(7-j)] = PURPLE
+			
+			strip.show()
+
+
